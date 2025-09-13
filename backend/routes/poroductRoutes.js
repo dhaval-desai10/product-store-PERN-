bfd9dcdf3d98cec70d4f75052e9529f5691b06e6
Dhaval Desai <dhavaldesai7000@gmail.com>
2025-09-13 18:08:18 +0530

    Implement CRUD operations for products in the product controller and update routes accordingly

--- backend/routes/poroductRoutes.js
+++ backend/routes/poroductRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createProduct, getAllProducts } from "../controllers/product.Controller.js";
+import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from "../controllers/product.Controller.js";
 
 const router = express.Router();
 
-router.get("/",getAllProducts);
+router.get("/",getProducts);
+router.get("/:id",getProduct);
 router.post("/",createProduct);
+router.put("/",updateProduct);
+router.delete("/:id",deleteProduct);
+
 
 export default router;
